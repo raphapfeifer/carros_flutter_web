833b3f4b61f2487ce7606d7d3df388d41e688254
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-05-04 22:57:09 -0300

    Primeira tela

---
+++ build/app/intermediates/instant_app_manifest/debug/gen/com/example/carros_flutter_web/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.carros_flutter_web;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
