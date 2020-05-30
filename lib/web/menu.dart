131f12ff7c540aadcabaf0f5f70b529f0624e519
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-05-30 14:47:31 -0300

    Deixar menu selecionado eu ser clicado

--- lib/web/menu.dart
+++ lib/web/menu.dart
@@ -1,4 +1,3 @@
-
 import 'package:carros_flutter_web/app_model.dart';
 import 'package:carros_flutter_web/pages/carros_page.dart';
 import 'package:carros_flutter_web/pages/default_page.dart';
@@ -7,37 +6,68 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
+class ItemMenu {
+  String title;
+  IconData icon;
+  Widget page;
+
+  bool selected = false;
+
+  ItemMenu(this.title, this.icon, this.page);
+}
+
 class Menu extends StatefulWidget {
   @override
   _MenuState createState() => _MenuState();
 }
 
 class _MenuState extends State<Menu> {
+  List<ItemMenu> menus = [];
+
   @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        _itemMenu("Home", FontAwesomeIcons.home,DefaultPage()),
-        _itemMenu("Carros", FontAwesomeIcons.car,CarrosPage()),
-        _itemMenu("Usuários", FontAwesomeIcons.user,UsuariosPage())
-      ],
-    );
+  void initState() {
+    super.initState();
+
+    menus.add(ItemMenu("Home", FontAwesomeIcons.home, DefaultPage()));
+    menus.add(ItemMenu("Carros", FontAwesomeIcons.car, CarrosPage()));
+    menus.add(ItemMenu("Usuários", FontAwesomeIcons.user, UsuariosPage()));
   }
 
-  _itemMenu(String title,IconData icon,Widget page){
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: menus.length,
+        itemBuilder: (context, index) {
+          ItemMenu item = menus[index];
+
+          return _itemMenu(item);
+        });
+  }
+
+  _itemMenu(ItemMenu item) {
     return Material(
-      color: Colors.transparent,
+      color: item.selected ? Theme.of(context).hoverColor : Colors.transparent,
       child: InkWell(
         onTap: () {
           AppModel app = Provider.of<AppModel>(context, listen: false);
-          app.setPage(page);
+          app.setPage(item.page);
+
+          setState(() {
+            menus.forEach((item) => item.selected = false);
+
+            item.selected = true;
+          });
         },
         child: ListTile(
-          leading: Icon(icon),
-          title: Text(title),
+          leading: Icon(item.icon),
+          title: Text(
+            item.title,
+            style: TextStyle(
+                fontWeight:
+                    item.selected ? FontWeight.bold : FontWeight.normal),
+          ),
         ),
       ),
     );
   }
-
 }
