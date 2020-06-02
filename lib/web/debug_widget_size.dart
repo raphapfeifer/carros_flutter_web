30f1b381302b23ee29896041d957f278d7598a9f
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-06-02 00:10:53 -0300

    LayoutBuilder parte 2

---
+++ lib/web/debug_widget_size.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class DebugWidgetSize extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Center(
+          child: Text(">>> ${constraints.maxWidth}/${constraints.maxHeight} <<<"),
+        );
+      },
+    );;
+  }
+}
