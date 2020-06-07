fde77ab34a7e55d2950a4de97f060ec73c38a01e
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2020-06-07 16:22:44 -0300

    comunicação http

---
+++ lib/pages/carros/carros_page.dart
@@ -0,0 +1,76 @@
+
+import 'dart:async';
+
+import 'package:carros_flutter_web/pages/carros/carro.dart';
+import 'package:carros_flutter_web/pages/carros/carros_api.dart';
+import 'package:carros_flutter_web/web/web_utils.dart';
+import 'package:flutter/material.dart';
+
+class CarrosPage extends StatefulWidget {
+  @override
+  _CarrosPageState createState() => _CarrosPageState();
+}
+
+class _CarrosPageState extends State<CarrosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder<List<Carro>>(
+          future: CarrosApi.getCarros(context),
+          builder: (BuildContext context, AsyncSnapshot snaphot){
+            if(!snaphot.hasData){
+              return Center(child: CircularProgressIndicator(),);
+            }
+
+            List<Carro> carros = snaphot.data;
+
+            return _listView(carros);
+          },
+        )
+    );
+  }
+
+  _listView(List<Carro> carros) {
+    return GridView.builder(
+        itemCount: carros.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 1.5),
+        itemBuilder: (context, index) {
+          return LayoutBuilder(builder: (context, constraints) {
+            double fontSize = size(constraints.maxWidth * 0.1,
+                min: 10, max: Theme
+                    .of(context)
+                    .textTheme
+                    .body1
+                    .fontSize);
+
+            Carro carro = carros[index];
+
+            return Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                          maxWidth: 250
+                      ),
+                      child: Container(
+                        color: Colors.blue[50],
+                        child: Image.network(
+                           carro.urlFoto ?? "http://www.livroandroid.com.br/livro/carros/esportivos/Renault_Megane_Trophy.png"),
+                      ),
+                    ),
+                    Text(
+                      carro.nome ?? "",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: fontSize),
+                    )
+                  ],
+                ));
+          });
+        });
+  }
+}
--- lib/pages/default_page.dart
+++ lib/pages/default_page.dart
@@ -1,5 +1,4 @@
 import 'package:carros_flutter_web/web/debug_widget_size.dart';
-import 'package:carros_flutter_web/web/web_utils.dart';
 import 'package:flutter/material.dart';
 
 class DefaultPage extends StatefulWidget {
@@ -15,41 +14,8 @@ class _DefaultPageState extends State<DefaultPage> {
     );
   }
 
-  _body() {
-    return GridView.builder(
-        itemCount: 3,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 20,
-            childAspectRatio: 1.5),
-        itemBuilder: (context, index) {
-          return LayoutBuilder(builder: (context, constraints) {
-            double fontSize = size(constraints.maxWidth * 0.1,
-                min: 10, max: Theme.of(context).textTheme.body1.fontSize);
-
-            return Card(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxWidth: 250
-                  ),
-                  child: Container(
-                    color: Colors.blue[50],
-                    child: Image.network(
-                        "http://www.livroandroid.com.br/livro/carros/esportivos/Renault_Megane_Trophy.png"),
-                  ),
-                ),
-                Text(
-                  "$fontSize - Renault Megane RS Trophy",
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: fontSize),
-                )
-              ],
-            ));
-          });
-        });
+  _body(){
+    return DebugWidgetSize();
   }
+
 }
